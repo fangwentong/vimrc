faaed1bb5344f5662ef7c454c425a75fbd36c8ab
房文同 <fangwentong2012@gmail.com>
2014-12-10 13:36:49 +0800

    update

--- onekey.sh
+++ onekey.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y git
-git clone https://github.com/fangwentong/vimrc.git
+git clone https://github.com/fangwentong/vimrc.git vimrc
 cd vimrc
 git checkout -t origin/master
 sh -x install.sh
