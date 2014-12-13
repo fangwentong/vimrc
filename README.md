250a8f0e957d11f655c09d71006c4c64e5dd8bb6
房文同 <fangwentong2012@gmail.com>
2014-12-13 18:14:44 +0800

    update README

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 My vimrc
 ---
 
+这是我的vim配置
+
+
+### 安装
+
+```
+git clone https://github.com/fangwentong/vimrc.git vimrc && cd vimrc && sh install.sh
+```
 
--- onekey.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt-get update
-sudo apt-get install -y git
-git clone https://github.com/fangwentong/vimrc.git vimrc
-cd vimrc
-git checkout -t origin/master
-sh -x install.sh
-
