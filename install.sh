7aa3f91a4498164a16610975638d01393aa21971
房文同 <fangwentong2012@gmail.com>
2014-12-13 17:59:30 +0800

    修改安装脚本风格

--- install.sh
+++ install.sh
@@ -3,51 +3,105 @@
 BASE_DIR=`pwd`
 TODAY=`date +%Y%m%d%H%M%S`
 
-#配置Vim
-echo "\033[34m* 备份vim配置...\033[0m"
-for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.bundles
-do [ -L $i ] && unlink $i
-done
-for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.bundles
-do [ -e $i ] && mv $i $i.$TODAY
-done
+install_mac_os() {
+    brew install vim ctags
 
-echo "\033[34m* 重新配置Vim...\033[0m"
-ln -s $BASE_DIR/vimrc $HOME/.vimrc
-ln -s $BASE_DIR/vimrc.bundles $HOME/.vimrc.bundles
-ln -s $BASE_DIR/ $HOME/.vim
+    brew install python
+    pip install pyflakes pylint pep8
 
-echo "\033[034m* 安装插件...\033[0m"
-if [ ! -e $BASE_DIR/bundle/vundle ]
-then
-    echo "\033[034m* 安装Vundle...\033[0m"
-    git clone https://github.com/gmarik/vundle.git $BASE_DIR/bundle/vundle
-else
-    echo "\033[034m* 升级Vundle...\033[0m"
-    cd "$BASE_DIR/bundle/vundle" && git pull origin master
-fi
+    brew install node
+    npm install jshint -g
+    npm install jslint -g
+}
 
-echo "\033[034m* 使用Vundle安装/升级插件...\033[0m"
-system_shell=$SHELL
-export SHELL="/bin/sh"
-echo "正在从GitHub上下载插件..." >> $BASE_DIR/notice
-echo "请保持网络畅通，耐心等待 :-)" >> $BASE_DIR/notice
-vim -u $HOME/.vimrc.bundles $BASE_DIR/notice +BundleInstall! +BundleClean +qall
-rm $BASE_DIR/notice
+install_debain() {
+    sudo apt-get install -y clang cmake build-essential 2>> err.log
 
-echo "\033[034m* 编译YouCompleteMe...\033[0m"
-echo "\033[034m* 请保持网络畅通，这可能会花费较长时间\033[0m"
-echo "\033[034m* 编译会占用很多系统资源 :-)\033[0m"
-echo "\033[034m* 若安装失败，请先检查依赖关系，再手动安装\033[0m"
-cd $BASE_DIR/bundle/YouCompleteMe/
+    # 解决Ubuntu 14.04编译YCM时找不到libclang.so的问题
+    if [ ! -e /usr/lib/llvm-3.4/lib/libclang.so ]
+    then
+        sudo ln -sf /usr/lib/llvm-3.4/lib/libclang.so.1 /usr/lib/llvm-3.4/lib/libclang.so
+    elif [ -L /usr/lib/llvm-3.4/lib/libclang.so ]
+    then
+        sudo unlink /usr/lib/llvm-3.4/lib/libclang.so
+        sudo ln -sf /usr/lib/llvm-3.4/lib/libclang.so.1 /usr/lib/llvm-3.4/lib/libclang.so
+    fi
 
-if [ `which clang` ]    #检查系统搜素路径中是否安装了clang
-then
-    bash -x install.sh --clang-completer --system-libclang
-else
-    bash -x install.sh --clang-completer
-fi
+    sudo apt-get install -y vim ctags 2>> err.log
+    sudo apt-get install -y python-dev python-setuptools 2>> err.log
+    sudo easy_install pip
+    sudo pip install pyflakes pylint pep8
 
-export SHELL=$system_shell
-#Vim配置完成
+    sudo apt-get install -y nodejs-legacy npm 2>> err.log
+    sudo npm install jslint -g
+    sudo npm install jshint -g
+}
+
+install_fedora() {
+    sudo yum install vim -y
+    sudo yum install python-devel.x86_64
+    sudo yum groupinstall 'Development Tools'
+}
+
+install_vim() {
+    echo "\033[034m* Installing vim...\033[0m"
+    if [ `which brew` ]
+    then
+        install_mac_os
+    elif [ `which apt-get` ]
+    then
+        install_debain
+    elif [ `which yum` ]
+    then
+        install_fedora
+    fi
+}
+
+# Configure Vim
+link_vimrc() {
+    echo "\033[34m* Backing up vim configure...\033[0m"
+    for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.bundles; do [ -L $i ] && unlink $i; done
+    for i in $HOME/.vim $HOME/.vimrc $HOME/.vimrc.bundles; do [ -e $i ] && mv $i $i.$TODAY; done
+
+    echo "\033[34m* Setting up symlinks...\033[0m"
+    ln -s $BASE_DIR/vimrc $HOME/.vimrc
+    ln -s $BASE_DIR/vimrc.bundles $HOME/.vimrc.bundles
+    ln -s $BASE_DIR/ $HOME/.vim
+}
+
+# Install Plugins
+install_plugins() {
+    if [ ! -e $BASE_DIR/bundle/vundle ]; then
+        echo "\033[034m* Installing Vundle...\033[0m"
+        git clone https://github.com/gmarik/vundle.git $BASE_DIR/bundle/vundle
+    else
+        echo "\033[034m* Upgrading Vundle...\033[0m"
+        cd "$BASE_DIR/bundle/vundle" && git pull origin master
+    fi
+
+    echo "\033[034m* Upgrading/Installing plugins using Vundle...\033[0m"
+    echo "Downloading plugins from GitHub..." >>$BASE_DIR/notice
+    vim -u $HOME/.vimrc.bundles $BASE_DIR/notice +BundleInstall! +BundleClean +qall
+    rm $BASE_DIR/notice
+}
+
+# Compile YouCompleteMe
+compile_ycm() {
+    echo "\033[034m* Compiling YouCompleteMe...\033[0m"
+    cd $BASE_DIR/bundle/YouCompleteMe/
+    if [ `which clang` ]    #Check system clang
+    then
+        bash -x install.sh --clang-completer --system-libclang # use system clang
+    else
+        bash -x install.sh --clang-completer
+    fi
+}
+
+################# Start ########################
+install_vim
+link_vimrc
+install_plugins
+compile_ycm
+# Vim configure complete
 echo "\033[034m* Vim Configure completed!\033[0m"
+################ End ###########################
